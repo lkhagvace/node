caa307fcf260e9db9365f78c2d3fee0ce58d01bd
Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>
2024-01-12 20:22:42 +0800

    delete put 2 iig hiiw

--- express.js
+++ express.js
@@ -1,18 +1,41 @@
 const express = require("express");
-const { read } = require("./objectreader");
+const { readUsers1, create, update, dlt } = require("./objectreader");
+const cors = require("cors");
 
-const port = 8080;
+const PORT = 8080;
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
-app.get("/", (req, res) => {
-  res.send("heello its ace");
+app.get("/users", (req, res) => {
+  console.log(req.method);
+  const read = readUsers1();
+  res.send(read);
 });
 // app.get("/users", (req, res) => {
 //   res.send([{}]);
 // });
-app.post("/users", (req, res) => {
-  res.send("its me");
+// app.post("/users", (req, res) => {
+//   res.send(readUsers(res));
+// });
+app.post("/product", (req, res) => {
+  const post = create({
+    id: 30,
+    first_name: "lkhagva",
+  });
+  res.send(post);
 });
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
+app.put("/putting", (req, res) => {
+  const put = update(3, { last_name: "Orgilsaikhan" });
+  res.send(put);
+});
+app.delete("/deleting", (req, res) => {
+  const deleting = dlt(4);
+  res.send(deleting);
+});
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
 });
--- objectreader.js
+++ objectreader.js
@@ -1,6 +1,7 @@
 const fs = require("node:fs");
 var http = require("http");
 const { url } = require("node:inspector");
+const { json } = require("express");
 const server = http.createServer((req, response) => {
   console.log(req.method);
   const reqUrl = req.url;
@@ -45,15 +46,23 @@ const readUsers = (response) => {
     response.end(JSON.stringify(data));
   });
 };
-// const create = (data) => {
-//   fs.readFile("/Users/ace/desktop/node/input.json", "utf8", (err, json) => {
-//     if (err) {
-//       return console.error(err);
-//     }
-//     const prevdata = JSON.parse(json);
-//     prevdata.push(data);
-//   });
-// };
+const readUsers1 = () => {
+  const reading = fs.readFileSync("users.json", "utf-8");
+  const read = JSON.parse(reading);
+  return read;
+};
+const create = (data) => {
+  const creation = fs.readFileSync("users.json", "utf-8");
+  const datas = JSON.parse(creation);
+  // const newArr = datas.concat(data);
+  datas.push(data);
+  fs.writeFile("users.json", JSON.stringify(datas), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+  return datas;
+};
 // create({
 //   id: 11,
 //   first_name: "ace",
@@ -62,37 +71,30 @@ const readUsers = (response) => {
 //   gender: "Female",
 //   ip_address: "32.114.201.138",
 // });
-// const update = (id, data) => {
-//   fs.readFile("/Users/ace/desktop/node/input.json", "utf8", (err, json) => {
-//     if (err) {
-//       return console.error(err);
-//     }
-//     const prevdata = JSON.parse(json);
-//     const DataToUpdate = prevdata.filter((el) => el.id === id);
-//     const newArr = prevdata.filter((el) => el.id !== id);
-//     newArr.push({ ...{ DataToUpdate }, ...data });
-//     console.log(newArr);
-//     fs.writeFile(
-//       "/Users/ace/desktop/node/input.json",
-//       "utf8",
-//       JSON.stringify(newArr),
-//       (err) => {
-//         if (err) return console.error(err);
-//       }
-//     );
-//   });
-// };
+const update = (id, data) => {
+  const datas = fs.readFileSync("users.json", "utf-8");
+  const prevData = JSON.parse(datas);
+  const dataToUp = prevData.filter((info) => info.id === id);
+  const newArr = prevData.filter((indexes) => indexes.id !== id);
+  newArr.push({ ...dataToUp, ...data });
+
+  fs.writeFile("users.json", JSON.stringify(newArr), (err) => {
+    if (err) return console.error(err);
+  });
+  return newArr;
+};
+
 // update(1, { last_name: "Orgilsaikhan" });
-// const dlt = (id) => {
-//   fs.readFile("/Users/ace/desktop/node/input.json", "utf8", (err, json) => {
-//     if (err) {
-//       return console.error(err);
-//     }
-//     const prevdata = JSON.parse(json);
-//     const dltId = prevdata[id - 1];
-//     const newArr = prevdata.filter((data) => data.id !== dltId.id);
-//   });
-// };
+const dlt = (id) => {
+  const deleting = fs.readFileSync("users.json", "utf8");
+  const prevdata = JSON.parse(deleting);
+  const newArr = prevdata.filter((data) => data.id !== id);
+  fs.writeFile("users.json", JSON.stringify(newArr), (err) => {
+    if (err) console.error("error: ", err);
+  });
+  return newArr;
+};
+module.exports = { readUsers1, create, update, dlt };
 // dlt();
 const PORT = 8000;
 server.listen(PORT, () => {
